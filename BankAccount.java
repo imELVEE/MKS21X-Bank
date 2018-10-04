7e2148009b51f1e740d6ef463bfbede7b8aec922
Alvi Aziz <aaziz00@stuy.edu>
2018-10-04 01:10:25 -0400

    Made the initial file

---
+++ BankAccount.java
@@ -0,0 +1,44 @@
+public class BankAccount{
+	private double balance;
+	private int accountID;
+	private String password;
+
+	public BankAccount(double newBalance, int newAccountID, String newPassword) {
+		balance = newBalance;
+		accountID = newAccountID;
+		password = newPassword;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public int accountID() {
+		return accountID;
+	}
+
+	public void setPassword(String newPassword) {
+		password = newPassword;
+	}
+
+	public String toString() {
+		return accountID + "\t" + balance;
+	}
+
+	public boolean deposit(int amount) {
+		if (amount >= 0) {
+			balance += amount;
+			return true;
+		}
+		return false;
+	}
+		
+	public boolean withdraw(int amount) {
+		if (amount < balance) {
+			balance -= amount;
+			return true;
+		}
+		return false;
+	}
+
+}
